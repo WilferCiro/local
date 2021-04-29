e28e5b09e43390588509ce10922cb0b4dee4bb18
Wilfer Ciro <wilcirom@gmail.com>
2021-04-28 21:26:40 -0500

    URLS

---
+++ components/ConstantServer.js
@@ -0,0 +1,7 @@
+const ConstantServer = {
+	URL_public        : '/api/public/',
+	URL_private       : '/api/private/',
+	URL_server        : 'http://127.0.0.1:8000'
+};
+
+export default ConstantServer;
